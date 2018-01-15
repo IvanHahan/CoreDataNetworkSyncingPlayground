c13439dde91446178840bda481b902923ffbfc7f
Ivan Hahanov <hahanov@zfort.com>
2018-01-15 12:42:19 +0200

    setup managed object

--- CoreDataNetworkLayer/ViewController.swift
+++ CoreDataNetworkLayer/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+//    var fetchController: NSFetchedResultsController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+//extension ViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return fetchController
+//    }
+//}
+
 

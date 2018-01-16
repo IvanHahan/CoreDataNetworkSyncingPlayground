dd513ac538997116be9e18ae4c18f7f2074f6aac
Ivan Hahanov <hahanov@zfort.com>
2018-01-16 18:59:29 +0200

    add syncing department to server

--- CoreDataNetworkLayer/CoreDataNetworkLayer/DataController.swift
+++ CoreDataNetworkLayer/CoreDataNetworkLayer/DataController.swift
@@ -12,9 +12,9 @@ import CoreData
 class DataController {
     
     let container: NSPersistentContainer
-    private let sessionManager: SessionManager = SessionManager(baseUrl: "", config: URLSessionConfiguration.default)
+    private let sessionManager: SessionManager = SessionManager(baseUrl: "https://com.example.blat/", config: URLSessionConfiguration.default)
     
-    private var insertedObjects = Set<NSManagedObject>()
+    private var inserted: Department?
     
     init(completion: Closure<Void>? = nil) {
         container = NSPersistentContainer(name: "Company")
@@ -24,17 +24,19 @@ class DataController {
             }
             completion?(())
         }
-//        NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextObjectsDidChange,
-//                                               object: nil,
-//                                               queue: nil) { note in
-//                                                guard let context = note.object as? NSManagedObjectContext else { return }
-//                                                insertedObjects = context.insertedObjects
-//        }
-//        NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextDidSave,
-//                                               object: nil,
-//                                               queue: nil) { note in
-//                                                <#code#>
-//        }
+        NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextWillSave,
+                                               object: nil,
+                                               queue: nil) { [weak self] note in
+                                                guard let context = note.object as? NSManagedObjectContext else { return }
+                                                self?.inserted = context.insertedObjects.first { $0 is Department } as? Department
+        }
+        NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextDidSave,
+                                               object: nil,
+                                               queue: nil) { [weak self] note in
+                                                guard let department = self?.inserted else { return }
+                                                self?.sessionManager.execute(NetworkRequest.department.create(department: department))
+                                                self?.inserted = nil
+        }
     }
 
 }
---
+++ CoreDataNetworkLayer/CoreDataNetworkLayer/Network/NetworkRequest.swift
@@ -0,0 +1,61 @@
+//
+//  NetworkRequest.swift
+//  CoreDataNetworkLayer
+//
+//  Created by  Ivan Hahanov on 1/16/18.
+//  Copyright © 2018  Ivan Hahanov. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkRequest {
+    enum employee {
+        struct create: DecodableResultRequest {
+            
+            typealias Model = Employee
+            
+            let path: String = ""
+            let method: Method = .post
+            let body: Parameters? = [:]
+        }
+        
+        struct update: DecodableResultRequest {
+            
+            typealias Model = Employee
+            
+            let path: String = ""
+            let method: Method = .put
+            let body: Parameters? = [:]
+        }
+        
+        struct get: DecodableResultRequest {
+            
+            typealias Model = Employee
+            
+            let path: String = ""
+        }
+        
+        struct delete: DecodableResultRequest {
+            
+            typealias Model = Employee
+            
+            let path: String = ""
+            let method: Method = .delete
+        }
+    }
+    
+    enum department {
+        struct create: DecodableResultRequest {
+            
+            typealias Model = Department
+            
+            let path: String = ""
+            let method: Method = .post
+            let body: Parameters? = [:]
+            
+            init(department: Department) {
+                
+            }
+        }
+    }
+}

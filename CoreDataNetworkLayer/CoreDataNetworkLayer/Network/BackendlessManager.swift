36d4167eaa5d014a946bb8990dc8aee5ffab745d
Ivan Hahanov <hahanov@zfort.com>
2018-01-18 11:42:03 +0200

    create BackendlessManager

--- CoreDataNetworkLayer/CoreDataNetworkLayer/Network/BackendlessManager.swift
+++ CoreDataNetworkLayer/CoreDataNetworkLayer/Network/BackendlessManager.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 
-protocol BackendlessRequest {
-    var table: String { get }
-    var data: Parameters { get }
+enum BackendlessRequest<Model: Decodable> {
+    case save(object: Model, completion: ResultClosure<Void>)
+    case find(query: DataQueryBuilder, completion: ResultClosure<Model>)
+    case remove(object: Model, completion: ResultClosure<Void>)
 }
 
 class BackendlessManager {
@@ -23,11 +24,40 @@ class BackendlessManager {
         backendless.initApp("50F43BB7-8B2B-0509-FF7B-3665F066E500", apiKey: "665536C6-6AD9-86EF-FF87-349DACBBCF00")
     }
     
-    func save(_ request: BackendlessRequest, completion: ResultClosure<Void>) {
-//        backendless.data.ofTable(request.table).save(request.json,
-//                                                        response: <#T##((Any?) -> Void)!##((Any?) -> Void)!##(Any?) -> Void#>,
-//                                                        error: <#T##((Fault?) -> Void)!##((Fault?) -> Void)!##(Fault?) -> Void#>)
-        
+    func execute<Model>(_ request: BackendlessRequest<Model>, for table: String) {
+        switch request {
+        case .find(let query, let completion):
+            backendless.data.ofTable(table).find(query, response: { result in
+                do {
+                    let decoder = JSONDecoder()
+                    let model = try decoder.decode(Model.self, from: JSONSerialization.data(withJSONObject: result!, options: []))
+                    completion(.success(model))
+                } catch {
+                    completion(.failure(error))
+                }
+            }, error: { (error) in
+                completion(.failure(NSError(domain: error!.detail,
+                                            code: Int(error!.faultCode)!,
+                                            userInfo: [NSLocalizedDescriptionKey: error!.message])))
+                })
+        case .save(let object, let completion):
+            backendless.data.ofTable(table).save(object,
+                                                 response: { result in
+                                                    completion(.success(()))
+            }, error: { error in
+                completion(.failure(NSError(domain: error!.detail,
+                                            code: Int(error!.faultCode)!,
+                                            userInfo: [NSLocalizedDescriptionKey: error!.message])))
+                })
+        case .remove(let object, let completion):
+            backendless.data.ofTable(table).remove(object, response: { (id) in
+                
+            }, error: { (error) in
+                completion(.failure(NSError(domain: error!.detail,
+                                            code: Int(error!.faultCode)!,
+                                            userInfo: [NSLocalizedDescriptionKey: error!.message])))
+                })
+        }
     }
 }
 

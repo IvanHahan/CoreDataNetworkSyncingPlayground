9ba57d3d9027835c4e28428938ed9fe66dd4f014
Ivan Hahanov <ivanhahanov@icloud.com>
2018-02-19 17:53:40 +0200

    add skipping repeated states

--- CoreDataNetworkLayer/Controllers/DepartmentsViewController.swift
+++ CoreDataNetworkLayer/Controllers/DepartmentsViewController.swift
@@ -26,7 +26,9 @@ class DepartmentsViewController: UIViewController {
         tableView.register(UITableViewCell.self)
         
         store = DepartmentStore(reducer: departmentReducer, departmentRepository: (UIApplication.shared.delegate as! AppDelegate).departmentRepository)
-        store.subscribe(self)
+        store.subscribe(self) {
+            $0.skipRepeats { $0.departments.count == $1.departments.count }
+        }
         store.dispatch(DepartmentAction.fetchDepartments)
     }
     
@@ -44,8 +46,6 @@ class DepartmentsViewController: UIViewController {
         store.dispatch(DepartmentAction.selectDepartment(department: department))
         store.dispatch(DepartmentAction.createDepartment)
     }
-    
-    
 }
 
 extension DepartmentsViewController: StoreSubscriber {
